b70915513f001ba2891d1742d72988d5a1170e88
a12345 <wimm251280@icloud.com>
2022-05-15 12:56:16 +0300

    Initial commit

---
+++ src/main/java/com/example/comission/ComissionApplication.java
@@ -0,0 +1,13 @@
+package com.example.comission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComissionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComissionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/comission/ComissionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.comission;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComissionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
